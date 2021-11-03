0514b8911438a373f5b447f10b6e987887390fa2
anaharri <anaharringtonb@gmail.com>
2021-11-03 17:05:41 -0300

    rutas post /characters y get /:id

--- api/src/routes/characters.js
+++ api/src/routes/characters.js
@@ -1,7 +1,62 @@
 const { Router } = require('express')
-const { Character } = require('../db')
+const { Character, Episode } = require('../db')
 const axios = require('axios')
 const router = Router()
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params
+  let character
+  
+  try {
+    if (id.includes('-')) {
+      character = await Character.findByPk(id, { include: Episode })
+
+      character = {
+        id: character.id,
+        name: character.name,
+        image: character.image,
+        episodes: character.Episodes.map((episode) => {
+          return {
+            id: episode.id,
+            name: episode.name,
+          }
+        }),
+      }
+    } else {
+      let characterResponse = await axios.get(
+        `https://rickandmortyapi.com/api/character/${id}`
+      )
+      characterResponse = characterResponse.data
+
+      let episodesList = characterResponse.episode.map((episode) =>
+        episode.split('/').pop()
+      )
+
+      episodesList = episodesList.join(',')
+
+      let episodes = await axios.get(
+        `https://rickandmortyapi.com/api/episode/${episodesList}`
+      )
+      episodes = episodes.data.map((episodes) => {
+        return {
+          id: episodes.id,
+          name: episodes.name,
+        }
+      })
+
+      character = {
+        id: characterResponse.id,
+        name: characterResponse.name,
+        image: characterResponse.image,
+        episodes: episodes,
+      }
+    }
+
+    res.json(character)
+  } catch (error) {
+    next(error)
+  }
+})
 
 router.get('/', async (req, res, next) => {
   let apiCharacters
@@ -23,6 +78,32 @@ router.get('/', async (req, res, next) => {
     dbCharacters = await Character.findAll()
 
     res.json([...dbCharacters, ...apiCharacters])
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  const { name, image, episodes } = req.body
+
+  try {
+    let character = await Character.create({ name, image })
+    await character.setEpisodes(episodes)
+
+    let charWithEpisodes = await Character.findOne({
+      where: { name: name },
+      attributes: {
+        exclude: ['updatedAt', 'createdAt'],
+      },
+      include: {
+        model: Episode,
+        through: {
+          attributes: [],
+        },
+      },
+    })
+
+    res.json(charWithEpisodes)
   } catch (error) {
     next(error)
   }
