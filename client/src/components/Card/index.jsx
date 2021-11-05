e49d1dcad2868e50c592b99a79836f25574ac90e
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2021-11-04 21:51:02 -0300

    bug fix (key no es una prop)

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './Card.module.css'
 
-const index = ({ name, image, key }) => {
+const index = ({ name, image, id }) => {
   return (
     <div className={style.cardContainer}>
 
-    <Link className={style.link} to={`/character/${key}`}>
+    <Link className={style.link} to={`/character/${id}`}>
         <h2>Name: {name}</h2>
         <img src={image} alt={name} />
     </Link>
--- client/src/components/Cards/index.jsx
+++ client/src/components/Cards/index.jsx
@@ -5,7 +5,7 @@ import style from './Cards.module.css'
 const Cards = ({ characters }) => {
   return (
     <section className={style.cardWrapper}>
-     {characters?.map(character => <Card name={character.name} image={character.image} key={character.id} />)}
+     {characters?.map(character => <Card name={character.name} image={character.image} id={character.id} key={character.id} />)}
     </section>
   );
 };
